dd7900dfaabbc5dcfd5571256c5d0e344c3841ef
dmitrynosenko <43008444+dmitrynosenko@users.noreply.github.com>
2018-09-05 19:06:55 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,18 @@ MovePlayer = function (_id) {
     if (DOM.findById (_id).getValue () == ""){
         return 0;
     }
+    if (_Move == "firstPlayer") {
+        DOM.findById (_id).setValue ("x");
+        _Move = "secondPlayer";
+    }
+    else {
+        DOM.getElementById(_id).setValue ("0");
+        _Move = "firstPlayer";
+
+    }
 }
+
+document.getElementById(#f1).setValue
 
 
 
