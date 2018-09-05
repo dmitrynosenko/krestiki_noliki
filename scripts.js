b5fd0de4de812fc2d52645665e1f75a51a995d80
dmitrynosenko <43008444+dmitrynosenko@users.noreply.github.com>
2018-09-05 18:54:18 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+var _Move = "firstPlayer"
+MovePlayer = function (_id) {
+    if (DOM.findById (_id).getValue () == ""){
+        return 0;
+    }
+}
+
+
+
+
+
+
+
+//
+// if (DOM.getElementById(_id).getValue() == ""){
+//     return 0;
+// }
